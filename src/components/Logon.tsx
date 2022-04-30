9a5007385e07c51f632a13c622c388eeb211ce97
Michael Bradshaw <mjbradshaw87@gmail.com>
2022-04-30 18:15:12 -0500

    - handle error message for logon component

--- src/components/Logon.tsx
+++ src/components/Logon.tsx
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { Box, Button, TextInput} from 'grommet';
+import { Box, Button, TextInput, Text} from 'grommet';
 import { updateUsername, loadUser } from '../actions/user';
 import type { ConnectedProps } from 'react-redux';
 import type { RootState } from '../store';
@@ -13,13 +13,16 @@ const connector = connect(
 const UserLogon: FunctionComponent<ConnectedProps<typeof connector>> = ({
     username, updateUsername, loadUser, errorMessage
 }) => (
-    <Box direction="row" alignSelf="center" gap="medium" pad="25px">
-        <TextInput
-            onChange={(e) => updateUsername(e.target.value)}
-            placeholder="Sleeper Name"
-            value={username} 
-        />
-        <Button label="Get" onClick={loadUser} disabled={!username} />
+    <Box>
+        <Box direction="row" alignSelf="center" gap="medium" pad="25px">
+                <TextInput
+                    onChange={(e) => updateUsername(e.target.value)}
+                    placeholder="Sleeper Name"
+                    value={username} 
+                />
+            <Button label="Get" onClick={loadUser} disabled={!username} />
+        </Box>
+        {errorMessage && <Text alignSelf="center" color={{'light': 'status-critical'}}>{errorMessage}</Text>}
     </Box>
     );
 
